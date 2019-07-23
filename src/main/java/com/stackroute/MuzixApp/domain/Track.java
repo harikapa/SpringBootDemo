45c42b206600acb914f8a1acd92dfb788dce795b
harikapa <harikasweety610@gmail.com>
2019-07-23 23:44:07 +0530

    added lombok dependency

--- src/main/java/com/stackroute/MuzixApp/domain/Track.java
+++ src/main/java/com/stackroute/MuzixApp/domain/Track.java
@@ -1,9 +1,18 @@
 package com.stackroute.MuzixApp.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
 @Table(name = "Track")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Track {
 
     @Id
@@ -15,39 +24,39 @@ public class Track {
 
     @Column
     private String comment;
-
-    public Track()
-    {
-
-    }
-
-    public Track(int id, String name, String comment) {
-        this.id = id;
-        this.name = name;
-        this.comment = comment;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public void setComment(String comment) {
-        this.comment = comment;
-    }
+//
+//    public Track()
+//    {
+//
+//    }
+//
+//    public Track(int id, String name, String comment) {
+//        this.id = id;
+//        this.name = name;
+//        this.comment = comment;
+//    }
+//
+//    public int getId() {
+//        return id;
+//    }
+//
+//    public void setId(int id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getComment() {
+//        return comment;
+//    }
+//
+//    public void setComment(String comment) {
+//        this.comment = comment;
+//    }
 }
